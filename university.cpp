1b3fa0413ca3d921d1a3602922e08e4e57e12677
Michał Antoniuk <michantoniuk@gmail.com>
2021-06-21 19:33:50 +0200

    implemented sortBySurname

--- university.cpp
+++ university.cpp
@@ -28,6 +28,13 @@ void University::sortByPesel() {
 
   sort->execute();
 }
+void University::sortBySurname() {
+  std::unique_ptr<Sort> sort(new Sort(this, [](Person *l, Person *p) {
+    return l->getSurname() < p->getSurname();
+  }));
+
+  sort->execute();
+}
 std::vector<Person *> University::getStudents() const {
     return students_;
 }
